8ae50606d4d6cfe5abca43b8f4f0d7cd8eeda6f0
lwincho-dev <lwinlwincho135324@gmail.com>
2022-10-06 23:38:11 -0700

    explor:This is navigation component.

--- app/src/main/java/com/llc/navigationcomponent/MainActivity.kt
+++ app/src/main/java/com/llc/navigationcomponent/MainActivity.kt
@@ -5,16 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.llc.navigationcomponent.databinding.ActivityMainBinding
 
 /**
  * Activity for cupcake order flow.
  */
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
 
         // Retrieve NavController from the NavHostFragment
         val navHostFragment = supportFragmentManager
